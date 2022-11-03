10ce307de2f90b300cab89bfb9899d6b0e0076b6
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-11-03 21:06:19 +0000

    add classes to color & sizes

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -4,14 +4,11 @@ import Product from '../Product/Product';
 
 const Products = () => {
   const [products] = useState(productsData);
-  // const [currentColor, setCurrentColor] = useState("white");
-  // const [currentPrice, setCurrentPrice] = useState("S");
-
 
   return (
     <section>
       {products.map((products) => (
-        <Product title={products.title} price={products.basePrice} key={products.name} id={products.id} color={products.colors}
+        <Product key={products.name}
           {...products}
         />
       ))}
